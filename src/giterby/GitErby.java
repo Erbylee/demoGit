69c57ba681ccd6b34ad2e2fa7f74e70e23e41e82
Erbylee <erbylee@gmail.com>
2015-01-22 23:40:06 -0300

    Ajouter un autre affiche

--- src/giterby/GitErby.java
+++ src/giterby/GitErby.java
@@ -17,6 +17,7 @@ public class GitErby {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Git de Erby");
+        System.out.println(" God is good ");
     }
     
 }
